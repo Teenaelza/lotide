2989c8994b15a641b633b90fc451e0ac514c0087
Teenaelza <teena.thaliath@gmail.com>
2021-08-17 02:12:41 +0000

    used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,10 @@ const assertEqual = function(actual, expected) {
   let emoji = "";
   if (actual === expected) {
     emoji = String.fromCodePoint(128512)  + String.fromCodePoint(128077);
-    console.log(emoji + "Assertion Passed: [" + actual  + "] === [" + expected + "]");
+    console.log(`${emoji} Assertion Passed: [${actual}] === [${expected}]`);
   } else {
     emoji = String.fromCodePoint(128543)  + String.fromCodePoint(128078);
-    console.log(emoji + "Assertion Failed: [" +  actual  + "] !== ["  + expected + "]");
+    console.log(`${emoji} Assertion Failed:[ ${actual}] !== [${expected}]`);
   }
 };
 
