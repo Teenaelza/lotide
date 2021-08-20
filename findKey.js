25211ffe6eab13b96b569eb291dd2431dc8ee9af
Teenaelza <teena.thaliath@gmail.com>
2021-08-20 03:04:24 +0000

    implemented the find key functionality

---
+++ findKey.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let emoji = "";
+  if (actual === expected) {
+
+    emoji  = "✅✅✅✅✅";
+    console.log(`${emoji} Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    emoji  = "🚩🚩🚩🚩";
+    console.log(`${emoji} Assertion Failed:[ ${actual}] !== [${expected}]`);
+  }
+};
+
+//ACTUAL FUNCTION
+const findKey = (Obj,callBack) => {
+  for (let key in Obj) {
+    if (callBack(Obj[key])){
+      return key
+    }
+  }
+};
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) );// => "noma"
