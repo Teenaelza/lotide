734658479c7a34ec7b85b1d17c69ddb699424104
Teenaelza <teena.thaliath@gmail.com>
2021-08-24 01:06:59 +0000

    refactored using chai and mocha assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,19 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-console.log("-----Implement middle assertions---------")
-assertArraysEqual(middle([1, 2, 3]), [2]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => should PASS
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => should PASS
+describe('#middle',() => {
+  it("returns [1,2,3] for [2]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]); 
+  });
+  it("returns [1, 2, 3, 4, 5] for [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]); 
+  });
+  it("returns [1, 2] for []", () => {
+    assert.deepEqual(middle([1, 2]),[]); 
+  });
+  it("returns [1] for []", () => {
+    assert.deepEqual(middle([1]),[]); 
+  });
+  it("returns [1, 2, 3, 4] for [2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2,3]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,21 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-console.log("------tail---------")
-//assertion for tail.js
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log("------tail check if the original array is affected after implementing tail---------")
-// edge Case1: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// edge Case2: Check with one array value
-const result1 = tail(["Hello"]);
-assertEqual(result1.length, 0); // ensure first element is empty
-// edge Case2: Check with no value
-const result2 = tail([]);
-assertEqual(result2.length,0); // ensure first element is empty
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("#tail", () => {
+  it("returns ['Lighthouse','Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+  it("original array should not be affected", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words, words); 
+  }); 
+  it("returns [1,2,3,4,5] for [2,3,4,5]", () => {
+    assert.deepEqual(tail([1,2,3,4,5]),[2,3,4,5]); 
+  });
+});
