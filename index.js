abf0381c8eef5ce94703060db128bb74b93e283b
Teenaelza <teena.thaliath@gmail.com>
2021-08-24 02:46:40 +0000

    exporting all functions to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle   = require('./middle');
+const countLetters   = require('./countLetters');
+module.exports = { head,tail,middle,countLetters};
